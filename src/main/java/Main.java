eaf23c41923b0680945f5db81a39b3df873c271a
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-21 10:07:18 -0500

    Maven exercises done

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        System.out.println(StringUtils.swapCase(input));
+
+        System.out.println("Is Numeric: " + StringUtils.isNumericSpace(input));
+
+        System.out.println(StringUtils.reverse(input));
+
+
+    }
+    }
